c3bdb1a677e57df86d9087397b9b79f750739ac6
WinterHana <haha920@naver.com>
2023-02-14 18:53:14 +0900

    test

--- client.cpp
+++ client.cpp
@@ -2,39 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 
+// 리눅스 환경의 헤더
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
 void error_handling(char *message);
 
+// main의 매개변수 각각에 주목 argc : 매개변수 개수
 int main(int argc, char *argv[]) {
 
 	// 서버, 클라이언트 소켓 파일 디스크립터 변수 선언
 	int sock;
 
-	// sockaddr_in 구조체 변수 선언
+	// sockaddr_in 구조체 변수 선언 : 리눅스에서 사용
 	struct sockaddr_in server_addr;
 	char message[30];
 	int str_len;
 
+	// (설명하기 위한 로직) 사용 제한
 	if (argc != 3) {
 		printf("Usage: %s <IP> <port>\n", argv[0]);
-		exit(1);
+		exit(1);		// 종료
 	}
 
+	// ====<<기본 셋팅>>====
 	// 소켓 생성
 	// 소켓을 생성하는 순간에는 서버 소켓과 클라이언트 소켓으로 나뉘지 않는다                                                                                        .
 	// bind listen함수의 호출이 이어지면 서버 소켓, connect 함수의 호출로 이어지면 클라이언트 소켓
 	sock = socket(PF_INET, SOCK_STREAM, 0);
 	if (sock == -1)
-		error_handling("socket() error");
+		error_handling("socket() error");		// error_handling _ 창환씨 직접 제작...
 
 	// 서버 정보 초기화 및 정보 할당
 	memset(&server_addr, 0, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;						// AF_INET - IPv4를 쓴다는 의미
-	server_addr.sin_addr.s_addr = inet_addr(argv[1]);		// 연결할 서버 IP 설정
+	server_addr.sin_addr.s_addr = inet_addr(argv[1]);		// 연결할 서버 IP 설정. argv[1](매개변수 값)이 들어감 
 	server_addr.sin_port = htons(atoi(argv[2]));			// Port 설정
+	// ====<<기본 셋팅 끝>>====
+
 
 	// 서버에 connect 요청을 하고, 그에 따른 결과값을 저장
 	// -1인 경우 연결 오류 발생
@@ -42,19 +48,24 @@ int main(int argc, char *argv[]) {
 	if (connect_result == -1)
 		error_handling("connect() error");
 
-	// 서버로부터 데이터를 읽고, 데이터 내용과 길이를 저장함
-	str_len = read(sock, message, sizeof(message) - 1);
-	if (str_len == -1)
+	// 서버로 데이터 보냄
+	char data[30] = "test_message"
+	write(sock, data, sizeof(data));
+
+	// 서버로부터 데이터를 읽고, 데이터 내용과 길이를 저장함(-1은 맨 뒤의 문제 제외를 위함)
+	char data2[30];			// 서버로부터 받은 데이터 저장
+	str_len = read(sock, data2, sizeof(data2) - 1);
+	if (str_len == -1)		// 데이터가 정상적으로 받지 못했을 때
 		error_handling("read() error");
 
 	// 메시지를 출력함
-	printf("Message from server:%s\n", message);
+	printf("Message from server:%s\n", data2);
 
 	// 서버에 데이터를 보냄
-	message[0] = 'T';
-	message[1] = 'E';
-	message[2] = '\0';
-	write(sock, message, sizeof(message));
+	// message[0] = 'T';
+	// message[1] = 'E';
+	// message[2] = '\0';
+	// write(sock, message, sizeof(message));
 
 	// 소켓 연결 종료
 	close(sock);
--- server.cpp
+++ server.cpp
@@ -38,13 +38,14 @@ int main(int argc, char *argv[]) {
 	server_addr.sin_family = AF_INET;					// AF_INET - IPv4를 쓴다는 의미
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);	// INADDR_ANY는 자동으로 이 컴퓨터에 존재하는 랜카드 중 사용가능한 랜카드의 IP주소를 사용하라는 의미
 	server_addr.sin_port = htons(atoi(argv[1]));		// Port 설정
-
+	// ====<<기본 셋팅 끝>>====
+	
 	// bind 함수호출을 통해서 IP 주소와 Port 번호를 할당
 	int bind_result = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
 	if (bind_result == -1)
 		error_handling("bind() error");
 	
-	// listen()으로 서버소켓으로 오는 클라이언트 요청 대기
+	// listen()으로 서버소켓으로 오는 클라이언트 요청 대기 : 계속 listen() 중...
 	int listen_result = listen(server_sock, 5);
 	if (listen_result == -1)
 		error_handling("listen() error");
@@ -52,25 +53,39 @@ int main(int argc, char *argv[]) {
 	// 연결 요청의 수락을 위한 accept 함수를 호출하고 있음
 	// 연결 요청이 없는 상태에서 이 함수가 호출되면, 연결요청이 있을 때까지 함수는 반환하지 않음
 	client_addr_size = sizeof(client_addr);
-	client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_addr_size);
+	client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_addr_size);	// 데이터를 받고...
+
 	if (client_sock == -1)
 		error_handling("accept() error");
 
-	// write 함수는 데이터를 전송하는 기능의 함수인데, 이 문장이 실행됬다는 것은 연결요청이 있었다는 뜻
-	write(client_sock, message, sizeof(message));
-
-	// 클라이언트로부터 데이터를 읽고, 데이터 내용과 길이를 저장함
-	message[0] = '\0';
-	int str_len = read(client_sock, message, sizeof(message) - 1);
+	// 클라이언트로부터 데이터를 읽고, 데이터 내용과 길이를 저장함	// 서버 소켓 > 클라이언트 소켓으로 나뉨
+	char data[30];										
+	int str_len = read(client_sock, data, sizeof(data) - 1);	// null 문자 제거
 	if (str_len == -1)
 		error_handling("read() error");
 
 	// 메시지를 출력함
-	printf("Message from client:%s\n", message);
+	printf("클라이언트에게 받음:%s\n", data);
+
+	// write 함수는 데이터를 전송하는 기능의 함수인데, 이 문장이 실행됬다는 것은 연결요청이 있었다는 뜻
+	data[0] = 'Z';
+	data[1] = 'H';
+	data[2] = 'R';
+	data[3] = 'A';
+	write(client_sock, data, sizeof(data));
+
+	//// 클라이언트로부터 데이터를 읽고, 데이터 내용과 길이를 저장함	// 서버 소켓 > 클라이언트 소켓으로 나뉨
+	//message[0] = '\0';												// 날빌 : 맨 앞에 null를 넣으면 다 비워짐
+	//int str_len = read(client_sock, message, sizeof(message) - 1);	// null 문자 제거
+	//if (str_len == -1)
+	//	error_handling("read() error");
+
+	// 메시지를 출력함
+	//printf("Message from client:%s\n", message);
 
 	// 소켓 닫음
 	close(client_sock);
-	close(server_sock);
+	close(server_sock);		// listen을 중간에 while문을 통해 살릴 수도 있음
 
 	return 0;
 }
